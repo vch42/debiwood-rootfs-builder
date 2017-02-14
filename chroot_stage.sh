44eb8f387c1126aed9f28e0008f63f36cda0b43b
vch86 <ghub@cheche.ro>
2017-02-14 12:58:25 +0200

    .

--- chroot_stage.sh
+++ chroot_stage.sh
@@ -419,7 +419,8 @@ echo " "; sleep 5
 echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-set-selections
 echo iptables-persistent iptables-persistent/autosave_v6 boolean true | debconf-set-selections
 apt-get install -y $packs
-update-initramfs -u
+echo 'DEVICE /dev/sd?*' >> /etc/mdadm/mdadm.conf
+update-initramfs -u -k all
 
 
 
