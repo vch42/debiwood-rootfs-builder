d28bfc64f6e6fab4dee6047c5c1570189ef2f507
vch86 <ghub@cheche.ro>
2017-02-24 16:16:02 +0200

    nsa320_dtb_chip_delay_0x28

--- main_stage.sh
+++ main_stage.sh
@@ -60,7 +60,7 @@ tar xf  ./stuff/kern/$kernel/extracted/*.tar -C ./stuff/kern/$kernel/extracted
 \cp -rp ./stuff/kern/$kernel/extracted/dts $targetdir/boot/
 \rm -rf ./stuff/kern/$kernel/extracted
 
-if $custom_dtb_nsa320_vch; then
+if $nsa320_dtb_chip_delay_0x28; then
 	\cp -p $targetdir/boot/dts/kirkwood-nsa320.dts $targetdir/boot/dts/kirkwood-nsa320.dts.orig
     dtc -I dtb -O dts -o $targetdir/boot/dts/kirkwood-nsa320.dts $targetdir/boot/dts/kirkwood-nsa320.dtb
     sed -ie 's/chip-delay = <0x23>;/chip-delay = <0x28>;/' $targetdir/boot/dts/kirkwood-nsa320.dts
