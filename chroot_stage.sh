0d295c9b968afedab040921e95ffbe60993b9d0c
vch42 <vch42@example.org>
2022-07-12 21:17:44 +0300

    Adding option to disable IPv6 and set default to disabled

--- chroot_stage.sh
+++ chroot_stage.sh
@@ -387,6 +387,19 @@ systemctl enable leds.service
 
 
 
+if $disable_ipv6; then
+	echo;echo;echo '*****************************************************'
+	echo "Disabling IPv6"
+	echo " "; sleep 5
+	cat<<EOT >> /etc/sysctl.conf
+net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv6.conf.default.disable_ipv6 = 1
+net.ipv6.conf.lo.disable_ipv6 = 1
+EOT
+
+
+
+
 echo;echo;echo '*****************************************************'
 echo "Installing the rest of the packages"
 echo " "; sleep 5
